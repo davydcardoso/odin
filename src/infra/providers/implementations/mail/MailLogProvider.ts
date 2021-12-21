9cd67d13dc0eed75de3b2c4d8a7d7291b5390de3
davydcardoso <dcsistemas3@gmail.com>
2021-12-21 00:32:48 -0300

    fix: correction auth login process

---
+++ src/infra/providers/implementations/mail/MailLogProvider.ts
@@ -0,0 +1,14 @@
+import { IMailProvider, MailMessage } from "../../models/IMailProvider";
+
+export class MailLogProvider implements IMailProvider {
+  async sendEmail(
+    message: MailMessage,
+    meta?: Record<string, unknown>
+  ): Promise<void> {
+    console.log({
+      message: message.subject,
+      recipient: message.to.email,
+      meta,
+    });
+  }
+}
